9ffab48af2c72830ff2a0997bd2c7c4badb4ab14
poolstudioapps <poolstudio.apps@gmail.com>
2026-09-09 14:24:57 +0200

    feat: add plant catalog and content angles

---
+++ lib/plants.ts
@@ -0,0 +1,37 @@
+export const PLANTS = [
+  { slug: "monstera-deliciosa", name: "Monstera deliciosa" },
+  { slug: "pothos", name: "Pothos" },
+  { slug: "snake-plant", name: "Snake Plant" },
+  { slug: "fiddle-leaf-fig", name: "Fiddle Leaf Fig" },
+  { slug: "peace-lily", name: "Peace Lily" },
+  { slug: "rubber-plant", name: "Rubber Plant" },
+  { slug: "zz-plant", name: "ZZ Plant" },
+  { slug: "spider-plant", name: "Spider Plant" },
+  { slug: "aloe-vera", name: "Aloe Vera" },
+  { slug: "calathea", name: "Calathea" },
+  { slug: "philodendron", name: "Philodendron" },
+  { slug: "string-of-pearls", name: "String of Pearls" },
+  { slug: "pilea", name: "Pilea" },
+  { slug: "orchid", name: "Orchid" },
+  { slug: "jade-plant", name: "Jade Plant" },
+  { slug: "begonia", name: "Begonia" },
+  { slug: "prayer-plant", name: "Prayer Plant" },
+  { slug: "bird-of-paradise", name: "Bird of Paradise" },
+  { slug: "areca-palm", name: "Areca Palm" },
+  { slug: "english-ivy", name: "English Ivy" },
+] as const;
+
+export const ANGLES = [
+  "Complete care guide",
+  "Watering guide",
+  "Best light and placement",
+  "Why are the leaves turning yellow?",
+  "5 common mistakes",
+  "Repotting guide",
+  "Humidity tips",
+  "Fertilizing guide",
+  "Pruning guide",
+  "Common pests and problems",
+] as const;
+
+export const APP_LINK = "https://plenova.onelink.me/nQZF/0f2sata";
